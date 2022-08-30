79e87862a80f8494245892d392b3cb2129e89d42
LinKaiRu <10746041@ntub.edu.tw>
2022-08-30 14:33:50 +0800

    header modify

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -5,41 +5,46 @@ export const HeaderBox = styled.div`
     align-items:center;
     justify-content:center;
     width:100%;
-    height:3.5rem;
-    background:#111;
+    height:60px;
+    background:rgb(0,0,0);
+    border-bottom: 1px solid rgb(54,54,54);
 `
 export const Container = styled.div`
-    width:90%;  
-    display:grid;
-    margin-left:17rem;
-    grid-template-columns: 18rem auto 33rem;
+    max-width: calc(935px + 40px);
+    width:100%;  
+    justify-content: space-between;
+    display:flex;
     color:white;
 `
 
-export const Title = styled.span`
+export const Title = styled.div`
+
     font-size:1.5rem;
 `
 export const SeachBox = styled.div`
     display:flex;
+    margin-left: 7rem;
 `
 export const PlaceholderBox = styled.div`
     position:absolute;
     width:15rem;
     height:2rem;
+    border-radius: 8px;
+    background: rgb(38,38,38);
     svg{
         position:relative;        
         font-size:1.2rem;
         top: 50%;
         transform: translateY(-50%);
         left:1rem;
-        color:#111;
+        color:rgb(155,155,155);
     }
 `
 export const Placeholder = styled.div`
     display:flex;
     position:absolute;
     left:2.5rem;
-    color:#111;
+    color:rgb(155,155,155);
     top: 50%;
     transform: translateY(-50%);
     white-space:nowrap;
@@ -51,10 +56,12 @@ export const Placeholder = styled.div`
 export const Search = styled.input`
     width:15rem;
     height:2rem;
-    border-radius:5px;
+    border-radius:8px;
     border:none;
     padding-left:1rem;
-    color:white;
+    background:rgb(38,38,38);
+    outline: none;
+    color:rgb(250,250,250);
     &::placeholder{
         font-size:1.2rem;
         display:flex;
@@ -71,21 +78,18 @@ export const Search = styled.input`
         color:#111;
         &::placeholder{       
         opacity:1;
-     }
-    
 
+     }
     }
     
 `
 
 export const Icon = styled.div`
     display:flex;
-    gap:.75rem;
+    gap:1.5rem;
     cursor: pointer;
     svg{
         width:1.75rem;
         height:1.75rem;
-        /* color:black; */
-        /* color:${({ toggle }) => toggle ? 'white' : ''}; */
     }
 `
